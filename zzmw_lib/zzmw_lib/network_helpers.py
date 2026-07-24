88a671999e4cb515a1b8d0190eb150df3ac5055d
Nico B <nicolasbrailo@gmail.com>
2026-07-24 09:59:50 +0100

    Sock reuse on restart

--- zzmw_lib/zzmw_lib/network_helpers.py
+++ zzmw_lib/zzmw_lib/network_helpers.py
@@ -39,6 +39,7 @@ def is_port_available(host, port):
     """
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((host, port))
             return True
     except OSError:
